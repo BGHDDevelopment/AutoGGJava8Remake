cb4bfc9b1a4537246b8b6c42b5eb1662fc73e9c6
StephanBH <bghdpayments@gmail.com>
2019-05-03 18:44:39 -0700

    Fixed mod info not showing.

--- src/main/java/me/boomboompower/autogg/AutoGG.java
+++ src/main/java/me/boomboompower/autogg/AutoGG.java
@@ -30,7 +30,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 @Mod(modid = AutoGG.MODID, version = AutoGG.VERSION, acceptedMinecraftVersions="[1.8.9]")
 public class AutoGG {
 
-    public static final String MODID = "AutoGG Remake";
+    public static final String MODID = "AutoGG Java 8 Remake";
     public static final String VERSION = "0.1";
 
     @Mod.Instance
